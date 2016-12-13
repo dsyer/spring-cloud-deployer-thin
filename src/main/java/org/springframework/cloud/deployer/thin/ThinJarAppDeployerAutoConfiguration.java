75dc8af9d74e3c3a1ca416bc84537a9381a1182d
Dave Syer <dsyer@pivotal.io>
2016-12-13 08:59:05 +0000

    Tested as a library and found a couple of tweaks

--- src/main/java/org/springframework/cloud/deployer/thin/ThinJarAppDeployerAutoConfiguration.java
+++ src/main/java/org/springframework/cloud/deployer/thin/ThinJarAppDeployerAutoConfiguration.java
@@ -3,16 +3,13 @@ package org.springframework.cloud.deployer.thin;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.deployer.spi.app.AppDeployer;
-import org.springframework.cloud.deployer.spi.local.LocalDeployerProperties;
 import org.springframework.cloud.deployer.spi.task.TaskLauncher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 
 @Configuration
-@EnableConfigurationProperties(LocalDeployerProperties.class)
 @ConditionalOnClass(ThinJarAppDeployer.class)
 @AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
 public class ThinJarAppDeployerAutoConfiguration {
